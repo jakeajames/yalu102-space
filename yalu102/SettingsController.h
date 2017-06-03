ba6b63765fb4e20af5a0a5d59f68b402b379e95d
jakeajames <jakeashacks@gmail.com>
2017-06-03 12:58:28 +0200

    Add files via upload

---
+++ yalu102/SettingsController.h
@@ -0,0 +1,16 @@
+//
+//  SettingsController.h
+//  yalu102
+//
+//  Created by Jake James on 4/22/17.
+//  Copyright © 2017 kimjongcracks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SettingsController : UIViewController {
+    IBOutlet UISwitch *esub;
+    IBOutlet UISwitch *recydia;
+}
+
+@end
---
+++ yalu102/SettingsController.m
@@ -0,0 +1,41 @@
+//
+//  SettingsController.m
+//  yalu102
+//
+//  Created by Jake James on 4/22/17.
+//  Copyright © 2017 kimjongcracks. All rights reserved.
+//
+
+#import "SettingsController.h"
+
+@interface SettingsController ()
+
+@end
+
+@implementation SettingsController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+- (void)dealloc {
+    [_esub release];
+    [super dealloc];
+}
+@end
